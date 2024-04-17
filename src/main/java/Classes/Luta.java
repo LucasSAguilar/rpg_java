c67557cc8a60c17755371fb0bd2fc316a98b29c9
Lucas Aguilar <lucassaguilar01@gmail.com>
2024-04-17 11:08:08 -0300

    funcao para calcular acerto

--- src/main/java/Classes/Luta.java
+++ src/main/java/Classes/Luta.java
@@ -1,6 +1,8 @@
 package Classes;
 
+import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Luta {
 
@@ -57,27 +59,68 @@ public class Luta {
 
     public void iniciarLuta() {
         Person jogadorSelecionado;
+        Person jogadorInativo;
+
         int numSelecao = definirPrimeiroAgir();
 
         if (numSelecao == 1) {
             jogadorSelecionado = lutadorUm;
+            jogadorInativo = lutadorDois;
         } else {
             jogadorSelecionado = lutadorDois;
+            jogadorInativo = lutadorUm;
         }
 
         while (lutadorUm.getVida() > 0 && lutadorDois.getVida() > 0) {
-            iniciarRodada(jogadorSelecionado);
+            Movimento movimentoEscolhido = definirMovimento(jogadorSelecionado);
+            boolean acertou = definirAcerto(jogadorInativo, movimentoEscolhido);
+            boolean teveAcertoCritico = definirAcertoCritico();
+            
             if (jogadorSelecionado == lutadorUm) {
                 jogadorSelecionado = lutadorDois;
+                jogadorInativo = lutadorUm;
+
             }
             if (jogadorSelecionado == lutadorDois) {
                 jogadorSelecionado = lutadorUm;
+                jogadorInativo = lutadorDois;
             }
         }
-       
+
     }
 
-    public void iniciarRodada(Person jogadorSelecionado) {
+    public Movimento definirMovimento(Person jogadorSelecionado) {
+        List<Movimento> movimentos = jogadorSelecionado.getMovimentos();
+        System.out.println("Escolha seu movimento, " + jogadorSelecionado.getNome());
+        for (int i = 0; i <= 2; i++) {
+            System.out.println(i + " - " + movimentos.get(i));
+        }
+        Scanner scanner = new Scanner(System.in);
+        int numEscolhido = scanner.nextInt();
+        return movimentos.get(numEscolhido);
+    }
+    
+
+    public boolean definirAcerto(Person atacado, Movimento movimento) {
+        boolean acertou = false;
+        int numDado = rodarDado();
+        int resultado = numDado - movimento.dificuldade_acerto;
+        if (resultado > atacado.CA) {
+            acertou = true;
+            System.out.println("--------- Você acertou em cheio! ---------");
+        } else {
+            System.out.println("--------- Que pena, você errou! ---------");
+        }
+        System.out.println("Os resultados foram:");
+        System.out.println("Valor no dado: " + numDado);
+        System.out.println("Dificuldade de acerto: " + movimento.getDificuldade_acerto());
+        System.out.println("CA do inimigo: " + atacado.getCA());
+        System.out.println("Valor final: " + resultado);
+
+        return acertou;
     }
 
+    public boolean definirAcertoCritico(){
+    return true;
+    }
 }
